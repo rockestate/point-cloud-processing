61dfa294d7d50c87667d0f7b47ef3dbda1fe011b
Mathieu Carette <mathieu@rockestate.be>
2018-02-04 04:46:41 +0100

    Make treetop search efficient

--- src/pcl_utils.py
+++ src/pcl_utils.py
@@ -1,34 +1,18 @@
-from scipy.spatial.kdtree import KDTree
 import numpy as np
 
-# Adapted from https://stackoverflow.com/a/27116281
-def locally_extreme_points(coords, data, radius, smoothing_radius = None, min_neighbourhood_size = -1, lookfor = 'max', p_norm = 2):
+def local_max(coords, radius, density_threshold=0):
     '''
-    Find local maxima of points in a pointcloud.  Ties result in both points passing through the filter.
-
-    Not to be used for high-dimensional data.  It will be slow.
-
-    coords: A shape (n_points, n_dims) array of point locations
-    data: A shape (n_points, ) vector of point values
-    radius: The (scalar) size of the neighbourhood in which to search.
-    min_neighbourhood_size: Extrema whose neighbourhood has less than min_neighbourhood_size points are discarded
-    lookfor: Either 'max', or 'min', depending on whether you want local maxima or minima
-    p_norm: The p-norm to use for measuring distance (e.g. 1=Manhattan, 2=Euclidian)
-
-    returns
-        filtered_coords: The coordinates of locally extreme points
-        filtered_data: The values of these points
+    Find local maxima of points in a pointcloud.
     '''
-    assert coords.shape[0] == data.shape[0], 'You must have one coordinate per data point'
-    extreme_fcn = {'min': np.min, 'max': np.max}[lookfor]
-    kdtree = KDTree(coords)
-    if smoothing_radius is not None:
-        smoothing_neighbours = kdtree.query_ball_tree(kdtree, r=smoothing_radius, p = p_norm)
-        smooth_data = np.array([np.mean(data[n]) for n in smoothing_neighbours])
-    else:
-        smooth_data = data
-    neighbours = kdtree.query_ball_tree(kdtree, r=radius, p = p_norm)
-    i_am_extreme = [(np.nonzero(np.equal(n,i))[0][0]==np.argmax(smooth_data[n])) & (len(n) > min_neighbourhood_size) for i, n in enumerate(neighbours)]
-    extrema, = np.nonzero(i_am_extreme)  # This line just saves time on indexing
-    return coords[extrema], data[extrema]
-
+    max_box = coords.copy()
+    for i in range(2):
+        max_box['X{}'.format(i)] = ((coords['X']/radius + i) /2).astype(int)*2
+        max_box['Y{}'.format(i)] = ((coords['Y']/radius + i) /2).astype(int)*2
+    max_box['X_'] = (coords['X']/radius).astype(int)
+    max_box['Y_'] = (coords['Y']/radius).astype(int)
+    for i in range(2):
+        for j in range(2):
+            max_box[str(i)+str(j)] = max_box.groupby(['X{}'.format(i), 'Y{}'.format(j)])['Z'].transform(np.max)
+    density = max_box.groupby(['X_','Y_'])['Z'].transform(len)
+    is_max = (max_box['00'] == max_box['10']) & (max_box['10'] == max_box['01']) & (max_box['01'] == max_box['11']) & (max_box['11'] == coords['Z'])
+    return coords[is_max & (density >= (density_threshold))]
